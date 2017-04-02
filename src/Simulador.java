9f676ea2e9f9ae0abaad144c2fb797dc2407b751
Rafaelle Amorim <rafaelle.xavier@gmail.com>
2017-04-02 11:50:18 -0300

    Adicionando diferentes tipos de hosts

--- src/Simulador.java
+++ src/Simulador.java
@@ -7,10 +7,16 @@ public class Simulador {
   public static void main(String[] args) {
     
     
-    //configurações do host
-    int numeroDemaquinas = 3;
-    double cpuHost = 0.9;
-    double memoHost = 0.9;
+    //configurações do host pequeno
+    int numeroDemaquinas_hostP = 3;
+    double cpuHost_hostP = 0.9;
+    double memoHost_hostP = 0.9;
+    
+  //configurações do host grande
+    int numeroDemaquinas_hostG = 3;
+    double cpuHost_hostG = 0.9;
+    double memoHost_hostG = 0.9;
+    
     
     //configurações das VMs
     double confMinima = 0.1;
@@ -18,21 +24,36 @@ public class Simulador {
     
     int requisicoes = 10000;
     
-    int reqAceitasFF = 0;
-    int reqNegadasFF = 0;
+    // Hosts pequenos
+    int reqAceitasFF_hostP = 0;
+    int reqNegadasFF_hostP = 0;
 
-    int reqAceitasBF = 0;
-    int reqNegadasBF = 0;
+    int reqAceitasBF_hostP = 0;
+    int reqNegadasBF_hostP = 0;
 
-    int reqAceitasRF = 0;
-    int reqNegadasRF = 0;
+    int reqAceitasRF_hostP = 0;
+    int reqNegadasRF_hostP = 0;
+    
+    // Hosts grandes
+    int reqAceitasFF_hostG = 0;
+    int reqNegadasFF_hostG = 0;
+
+    int reqAceitasBF_hostG = 0;
+    int reqNegadasBF_hostG = 0;
+
+    int reqAceitasRF_hostG = 0;
+    int reqNegadasRF_hostG = 0;
+
+    
+    Host hostFF_hostP = new Host(new Maquina(cpuHost_hostP, memoHost_hostP), numeroDemaquinas_hostP);
+    Host hostBF_hostP = new Host(new Maquina(cpuHost_hostP, memoHost_hostP), numeroDemaquinas_hostP);
+    Host hostRF_hostP = new Host(new Maquina(cpuHost_hostP, memoHost_hostP), numeroDemaquinas_hostP);
+    
+    Host hostFF_hostG = new Host(new Maquina(cpuHost_hostG, memoHost_hostG), numeroDemaquinas_hostG);
+    Host hostBF_hostG = new Host(new Maquina(cpuHost_hostG, memoHost_hostG), numeroDemaquinas_hostG);
+    Host hostRF_hostG = new Host(new Maquina(cpuHost_hostG, memoHost_hostG), numeroDemaquinas_hostG);
     
     
-    Host hostFF = new Host(new Maquina(cpuHost, memoHost), numeroDemaquinas);
-    Host hostBF = new Host(new Maquina(cpuHost, memoHost), numeroDemaquinas);
-    Host hostRF = new Host(new Maquina(cpuHost, memoHost), numeroDemaquinas);
-    
-
 	//Requisições
     for (int i = 0; i < requisicoes; i++) {
 
@@ -42,36 +63,70 @@ public class Simulador {
       double randomValue = confMinima + (confMaxima - confMinima) * r.nextDouble();
 
       Maquina maquina = new Maquina(randomValue,randomValue);
-
-      if(hostFF.aloqueFF(maquina)){
-        reqAceitasFF ++;
+      // Maquinas com mesma configuração para poder avaliar os diferentes hosts e algoritmos
+     
+      
+      //Hosts pequenos      
+      if(hostFF_hostP.aloqueFF(maquina)){
+        reqAceitasFF_hostP ++;
       }else{
-        reqNegadasFF ++;
+        reqNegadasFF_hostP ++;
       }
 
-      if(hostBF.aloqueBF(maquina)){
-        reqAceitasBF ++;
+      if(hostBF_hostP.aloqueBF(maquina)){
+        reqAceitasBF_hostP ++;
       }else{
-        reqNegadasBF ++;
+        reqNegadasBF_hostP ++;
       }
 
-      if(hostRF.aloqueRF(maquina)){
-        reqAceitasRF ++;
+      if(hostRF_hostP.aloqueRF(maquina)){
+        reqAceitasRF_hostP ++;
       }else{
-        reqNegadasRF ++;
+        reqNegadasRF_hostP ++;
       }
+      
+      // Hosts grandes
+      if(hostFF_hostG.aloqueFF(maquina)){
+          reqAceitasFF_hostG ++;
+        }else{
+          reqNegadasFF_hostG ++;
+        }
+
+        if(hostBF_hostG.aloqueBF(maquina)){
+          reqAceitasBF_hostG ++;
+        }else{
+          reqNegadasBF_hostG ++;
+        }
+
+        if(hostRF_hostG.aloqueRF(maquina)){
+          reqAceitasRF_hostG ++;
+        }else{
+          reqNegadasRF_hostG ++;
+        }
       
     }
     
-    System.out.println("requisicoes negadas pelo código Fist Fit:" + reqNegadasFF);
-    System.out.println("Fragmentacao do código First Fit: " + hostFF.calcularFrag());
+    // Hosts pequenos
+    System.out.println("requisicoes negadas pelo código Fist Fit:" + reqNegadasFF_hostP);
+    System.out.println("Fragmentacao do código First Fit: " + hostFF_hostP.calcularFrag());
 
-    System.out.println("requisicoes negadas pelo código Best Fit:" + reqNegadasBF);
-    System.out.println("Fragmentacao do código Best Fit: " + hostBF.calcularFrag());
+    System.out.println("requisicoes negadas pelo código Best Fit:" + reqNegadasBF_hostP);
+    System.out.println("Fragmentacao do código Best Fit: " + hostBF_hostP.calcularFrag());
 
-    System.out.println("requisicoes negadas pelo código Random Fit:" + reqNegadasRF);
-    System.out.println("Fragmentacao do código Random Fit: " + hostRF.calcularFrag());
+    System.out.println("requisicoes negadas pelo código Random Fit:" + reqNegadasRF_hostP);
+    System.out.println("Fragmentacao do código Random Fit: " + hostRF_hostP.calcularFrag());
 
+
+    // Hosts grandes
+    System.out.println("requisicoes negadas pelo código Fist Fit:" + reqNegadasFF_hostG);
+    System.out.println("Fragmentacao do código First Fit: " + hostFF_hostG.calcularFrag());
+
+    System.out.println("requisicoes negadas pelo código Best Fit:" + reqNegadasBF_hostG);
+    System.out.println("Fragmentacao do código Best Fit: " + hostBF_hostG.calcularFrag());
+
+    System.out.println("requisicoes negadas pelo código Random Fit:" + reqNegadasRF_hostG);
+    System.out.println("Fragmentacao do código Random Fit: " + hostRF_hostG.calcularFrag());
+  
   }
   
   
